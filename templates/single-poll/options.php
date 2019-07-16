d1ccc7ac008cac13a67e53d6771a76150352d3c8
Farhad Ali <mycodrops@gmail.com>
2019-07-16 11:32:29 +0600

    Poll Style Variation

--- templates/single-poll/options.php
+++ templates/single-poll/options.php
@@ -25,7 +25,7 @@ $options_style = 2;
 <?php endif; ?>
 
 <?php if( $options_style == 2 ) : ?>
-    <div class="ac-custom ac-checkbox ac-checkmark wpp-option-list-2">
+    <div class="wpp-custom wpp-checkmark wpp-option-list-2">
         <div class="wpp-option-single">
             <input type="checkbox" name="england" id="england">
             <label for="england">England</label>
@@ -35,20 +35,26 @@ $options_style = 2;
             <input type="checkbox" name="newzealand" id="newzealand">
             <label for="newzealand">New Zealand</label>
         </div>
+    </div>
+<?php endif; ?>
 
-        <div class="wpp-option-single">
-            <input type="checkbox" name="india" id="india">
-            <label for="india">India</label>
-        </div>
+<?php if( $options_style == 2 ) : ?>
+<!--    <div class="ac-custom ac-checkbox ac-checkmark wpp-option-list-2">-->
+<!--        <div class="wpp-option-single">-->
+<!--            <input type="checkbox" name="england" id="england">-->
+<!--            <label for="england">England</label>-->
+<!--        </div>-->
+<!---->
+<!--        <div class="wpp-option-single">-->
+<!--            <input type="checkbox" name="newzealand" id="newzealand">-->
+<!--            <label for="newzealand">New Zealand</label>-->
+<!--        </div>-->
+<!--    </div>-->
 
-        <div class="wpp-option-single">
-            <input type="checkbox" name="pakisthan" id="pakisthan">
-            <label for="pakisthan">Pakisthan</label>
-        </div>
-
-        <div class="wpp-option-single">
-            <input type="checkbox" name="australia" id="australia">
-            <label for="australia">Australia</label>
-        </div>
+    <div class="wpp-custom wpp-radio wpp-fill wpp-option-list-2">
+        <div class="wpp-option-single"><input id="r1" name="r1" type="radio"><label for="r1">Seamlessly visualize quality intellectual capital</label></div>
+        <div class="wpp-option-single"><input id="r2" name="r1" type="radio"><label for="r2">Collaboratively administrate turnkey channels</label></div>
+        <div class="wpp-option-single"><input id="r3" name="r1" type="radio"><label for="r3">Objectively seize scalable metrics</label></div>
+        <div class="wpp-option-single"><input id="r4" name="r1" type="radio"><label for="r4">Energistically scale future-proof core competencies</label></div>
     </div>
 <?php endif; ?>
