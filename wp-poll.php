5bcdffaed728ff5288da2d6fd3336f8116de8b16
Farhad Ali <mycodrops@gmail.com>
2019-09-30 11:13:02 +0600

    PB setting path issue fix

--- wp-poll.php
+++ wp-poll.php
@@ -69,10 +69,11 @@ class WPPollManager {
 	 */
 	function define_classes_functions() {
 
-//		$settings_path = str_replace( array( 'Pluginbazar/free/', 'Pluginbazar\free/' ), '', ABSPATH );
-//		include $settings_path . "PB-Settings/class-pb-settings.php";
+		$settings_path = str_replace( array( 'Pluginbazar/free/', 'Pluginbazar\free/' ), '', ABSPATH );
+		include $settings_path . "PB-Settings/class-pb-settings.php";
 
-		require_once( WPP_PLUGIN_DIR . 'includes/classes/class-pb-settings.php' );
+//		require_once( WPP_PLUGIN_DIR . 'includes/classes/class-pb-settings.php' );
+
 		require_once( WPP_PLUGIN_DIR . 'includes/classes/class-item-data.php' );
 		require_once( WPP_PLUGIN_DIR . 'includes/classes/class-poll-post-types.php' );
 		require_once( WPP_PLUGIN_DIR . 'includes/classes/class-functions.php' );
