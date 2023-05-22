e6a5025de9a01fe09659ab29470b781b7817dc6c
Masum <masumwp01@gmail.com>
2023-05-22 19:58:21 +0600

    tiles widget 2

---
+++ templates/archive-reviews/tiles-widget-2.php
@@ -0,0 +1,273 @@
+<?php
+/**
+ * Slider Widget - 2
+ */
+
+use WPDK\Utils;
+
+defined( 'ABSPATH' ) || exit;
+
+$styles = isset( $args['styles'] ) ? $args['styles'] : '1';
+if ( ! isset( $args['id'] ) ) {
+	return;
+}
+
+$poll_id              = $args['id'];
+$poll                 = liquidpoll_get_poll( $poll_id );
+$single_review_url    = $poll->get_permalink();
+$slider_button_styles = $args['slider_button'] ?? 1;
+$all_reviews          = $poll->get_poll_results();
+$all_reviews_rating   = array();
+$all_reviews_value    = 0;
+
+foreach ( $all_reviews as $review ) {
+
+	$polled_value = Utils::get_args_option( 'polled_value', $review );
+
+	if ( ! empty( $polled_value ) && $polled_value >= 0 && $polled_value <= 5 ) {
+		$polled_value = ( ( $polled_value / 0.5 ) % 2 !== 0 ) ? $polled_value + 0.5 : $polled_value;
+
+		$all_reviews_value                   += $polled_value;
+		$all_reviews_rating[ $polled_value ] = ( $all_reviews_rating[ $polled_value ] ?? 0 ) + 1;
+	}
+}
+
+$overall_rating = count( $all_reviews ) > 0 ? round( $all_reviews_value / count( $all_reviews ), 1 ) : 0;
+
+?>
+
+<div class="reviews-tiles style-<?php echo esc_attr( $styles ); ?>">
+    <div class="slider-heading-wrap">
+        <div class="slider-heading">
+            <span class="rate">Rated Excellent!</span>
+            <form class="review-stars">
+				<?php echo liquidpoll_get_review_stars( $overall_rating ); ?>
+            </form>
+            <div class="review-count">
+                <span>Based on</span>
+                <span class="count"><?php echo sprintf( esc_html__( '%s Reviews', 'wp-poll' ), count( $all_reviews ) ); ?></span>
+            </div>
+        </div>
+    </div>
+    <div class="reviews-wrap tiles-grid">
+
+		<?php foreach ( $poll->get_poll_results( array() ) as $poll_result ) : ?>
+
+			<?php
+			$result_id         = Utils::get_args_option( 'id', $poll_result );
+			$polled_value      = Utils::get_args_option( 'polled_value', $poll_result, 0 );
+			$polled_comments   = Utils::get_args_option( 'polled_comments', $poll_result );
+			$poller_id         = Utils::get_args_option( 'poller_id_ip', $poll_result );
+			$poller_user       = get_user_by( 'id', $poller_id );
+			$review_title      = liquidpoll_get_results_meta( $result_id, 'review_title' );
+			$poller_ip_address = liquidpoll_get_results_meta( $result_id, 'poller_ip_address' );
+			?>
+
+		<?php endforeach; ?>
+
+        <div class="single-review">
+            <div class="user">
+                <div class="user-avatar">
+                    <img src="<?php echo esc_url( LIQUIDPOLL_PLUGIN_URL . 'assets/images/author.png' ) ?>" alt="">
+                </div>
+                <div class="user-info">
+                    <span class="user-name">Miranda Ash-Patel</span>
+                    <div class="user-location">
+                        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 6.5C5.82843 6.5 6.5 5.82843 6.5 5C6.5 4.17157 5.82843 3.5 5 3.5C4.17157 3.5 3.5 4.17157 3.5 5C3.5 5.82843 4.17157 6.5 5 6.5Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M5 11C7 9 9 7.20914 9 5C9 2.79086 7.20914 1 5 1C2.79086 1 1 2.79086 1 5C1 7.20914 3 9 5 11Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span class="location">United Kingdom</span>
+                    </div>
+                </div>
+            </div>
+            <form class="review-stars">
+			    <?php echo liquidpoll_get_review_stars( 4 ); ?>
+            </form>
+            <div class="review-comment-heading">
+                <span class="comment-heading"><?php echo 'I’m impressed with the product and service!'; ?></span>
+            </div>
+            <p class="publish-time">2 days ago</p>
+            <div class="review-comment">
+			    <?php echo apply_filters( 'the_content', 'Couldn’t have had a better experience than I have with SUNS lifestyle from start to finish fast and efficient and kept informed every step of the way.The fitters Christian and Brent with absolutely brilliant, friendly and helpful and have done a fantastic job. Highly recommended.' ); ?>
+            </div>
+        </div>
+        <div class="single-review">
+            <div class="user">
+                <div class="user-avatar">
+                    <img src="<?php echo esc_url( LIQUIDPOLL_PLUGIN_URL . 'assets/images/author.png' ) ?>" alt="">
+                </div>
+                <div class="user-info">
+                    <span class="user-name">Miranda Ash-Patel</span>
+                    <div class="user-location">
+                        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 6.5C5.82843 6.5 6.5 5.82843 6.5 5C6.5 4.17157 5.82843 3.5 5 3.5C4.17157 3.5 3.5 4.17157 3.5 5C3.5 5.82843 4.17157 6.5 5 6.5Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M5 11C7 9 9 7.20914 9 5C9 2.79086 7.20914 1 5 1C2.79086 1 1 2.79086 1 5C1 7.20914 3 9 5 11Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span class="location">United Kingdom</span>
+                    </div>
+                </div>
+            </div>
+            <form class="review-stars">
+			    <?php echo liquidpoll_get_review_stars( 4 ); ?>
+            </form>
+            <div class="review-comment-heading">
+                <span class="comment-heading"><?php echo 'I’m impressed with the product and service!'; ?></span>
+            </div>
+            <p class="publish-time">2 days ago</p>
+            <div class="review-comment">
+			    <?php echo apply_filters( 'the_content', 'Couldn’t have had a better experience than I have with SUNS lifestyle from...The fitters Christian and Brent with absolutely brilliant, friendly and helpful and have done a fantastic job. Highly recommended.' ); ?>
+            </div>
+        </div>
+        <div class="single-review">
+            <div class="user">
+                <div class="user-avatar">
+                    <img src="<?php echo esc_url( LIQUIDPOLL_PLUGIN_URL . 'assets/images/author.png' ) ?>" alt="">
+                </div>
+                <div class="user-info">
+                    <span class="user-name">Miranda Ash-Patel</span>
+                    <div class="user-location">
+                        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 6.5C5.82843 6.5 6.5 5.82843 6.5 5C6.5 4.17157 5.82843 3.5 5 3.5C4.17157 3.5 3.5 4.17157 3.5 5C3.5 5.82843 4.17157 6.5 5 6.5Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M5 11C7 9 9 7.20914 9 5C9 2.79086 7.20914 1 5 1C2.79086 1 1 2.79086 1 5C1 7.20914 3 9 5 11Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span class="location">United Kingdom</span>
+                    </div>
+                </div>
+            </div>
+            <form class="review-stars">
+			    <?php echo liquidpoll_get_review_stars( 4 ); ?>
+            </form>
+            <div class="review-comment-heading">
+                <span class="comment-heading"><?php echo 'I’m impressed with the product and service!'; ?></span>
+            </div>
+            <p class="publish-time">2 days ago</p>
+            <div class="review-comment">
+			    <?php echo apply_filters( 'the_content', 'Couldn’t have had a better experience than I have with SUNS lifestyle from...' ); ?>
+            </div>
+        </div>
+        <div class="single-review">
+            <div class="user">
+                <div class="user-avatar">
+                    <img src="<?php echo esc_url( LIQUIDPOLL_PLUGIN_URL . 'assets/images/author.png' ) ?>" alt="">
+                </div>
+                <div class="user-info">
+                    <span class="user-name">Miranda Ash-Patel</span>
+                    <div class="user-location">
+                        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 6.5C5.82843 6.5 6.5 5.82843 6.5 5C6.5 4.17157 5.82843 3.5 5 3.5C4.17157 3.5 3.5 4.17157 3.5 5C3.5 5.82843 4.17157 6.5 5 6.5Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M5 11C7 9 9 7.20914 9 5C9 2.79086 7.20914 1 5 1C2.79086 1 1 2.79086 1 5C1 7.20914 3 9 5 11Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span class="location">United Kingdom</span>
+                    </div>
+                </div>
+            </div>
+            <form class="review-stars">
+			    <?php echo liquidpoll_get_review_stars( 4 ); ?>
+            </form>
+            <div class="review-comment-heading">
+                <span class="comment-heading"><?php echo 'I’m impressed with the product and service!'; ?></span>
+            </div>
+            <p class="publish-time">2 days ago</p>
+            <div class="review-comment">
+			    <?php echo apply_filters( 'the_content', 'Couldn’t have had a better experience than I have with SUNS lifestyle from...' ); ?>
+            </div>
+        </div>
+        <div class="single-review">
+            <div class="user">
+                <div class="user-avatar">
+                    <img src="<?php echo esc_url( LIQUIDPOLL_PLUGIN_URL . 'assets/images/author.png' ) ?>" alt="">
+                </div>
+                <div class="user-info">
+                    <span class="user-name">Miranda Ash-Patel</span>
+                    <div class="user-location">
+                        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 6.5C5.82843 6.5 6.5 5.82843 6.5 5C6.5 4.17157 5.82843 3.5 5 3.5C4.17157 3.5 3.5 4.17157 3.5 5C3.5 5.82843 4.17157 6.5 5 6.5Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M5 11C7 9 9 7.20914 9 5C9 2.79086 7.20914 1 5 1C2.79086 1 1 2.79086 1 5C1 7.20914 3 9 5 11Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span class="location">United Kingdom</span>
+                    </div>
+                </div>
+            </div>
+            <form class="review-stars">
+			    <?php echo liquidpoll_get_review_stars( 4 ); ?>
+            </form>
+            <div class="review-comment-heading">
+                <span class="comment-heading"><?php echo 'I’m impressed with the product and service!'; ?></span>
+            </div>
+            <p class="publish-time">2 days ago</p>
+            <div class="review-comment">
+			    <?php echo apply_filters( 'the_content', 'Couldn’t have had a better experience than I have with SUNS lifestyle from...' ); ?>
+            </div>
+        </div>
+        <div class="single-review">
+            <div class="user">
+                <div class="user-avatar">
+                    <img src="<?php echo esc_url( LIQUIDPOLL_PLUGIN_URL . 'assets/images/author.png' ) ?>" alt="">
+                </div>
+                <div class="user-info">
+                    <span class="user-name">Miranda Ash-Patel</span>
+                    <div class="user-location">
+                        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 6.5C5.82843 6.5 6.5 5.82843 6.5 5C6.5 4.17157 5.82843 3.5 5 3.5C4.17157 3.5 3.5 4.17157 3.5 5C3.5 5.82843 4.17157 6.5 5 6.5Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M5 11C7 9 9 7.20914 9 5C9 2.79086 7.20914 1 5 1C2.79086 1 1 2.79086 1 5C1 7.20914 3 9 5 11Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span class="location">United Kingdom</span>
+                    </div>
+                </div>
+            </div>
+            <form class="review-stars">
+			    <?php echo liquidpoll_get_review_stars( 4 ); ?>
+            </form>
+            <div class="review-comment-heading">
+                <span class="comment-heading"><?php echo 'I’m impressed with the product and service!'; ?></span>
+            </div>
+            <p class="publish-time">2 days ago</p>
+            <div class="review-comment">
+			    <?php echo apply_filters( 'the_content', 'Couldn’t have had a better experience than I have with SUNS lifestyle from...' ); ?>
+            </div>
+        </div>
+        <div class="single-review">
+            <div class="user">
+                <div class="user-avatar">
+                    <img src="<?php echo esc_url( LIQUIDPOLL_PLUGIN_URL . 'assets/images/author.png' ) ?>" alt="">
+                </div>
+                <div class="user-info">
+                    <span class="user-name">Miranda Ash-Patel</span>
+                    <div class="user-location">
+                        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 6.5C5.82843 6.5 6.5 5.82843 6.5 5C6.5 4.17157 5.82843 3.5 5 3.5C4.17157 3.5 3.5 4.17157 3.5 5C3.5 5.82843 4.17157 6.5 5 6.5Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M5 11C7 9 9 7.20914 9 5C9 2.79086 7.20914 1 5 1C2.79086 1 1 2.79086 1 5C1 7.20914 3 9 5 11Z" stroke="#8D8D8D" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span class="location">United Kingdom</span>
+                    </div>
+                </div>
+            </div>
+            <form class="review-stars">
+			    <?php echo liquidpoll_get_review_stars( 4 ); ?>
+            </form>
+            <div class="review-comment-heading">
+                <span class="comment-heading"><?php echo 'I’m impressed with the product and service!'; ?></span>
+            </div>
+            <p class="publish-time">2 days ago</p>
+            <div class="review-comment">
+			    <?php echo apply_filters( 'the_content', 'Couldn’t have had a better experience than I have with SUNS lifestyle from...' ); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    jQuery(window).on('load', function () {
+        jQuery('.tiles-grid').masonry({
+            // set itemSelector so .grid-sizer is not used in layout
+            itemSelector: '.single-review',
+            columnWidth: '.single-review',
+            fitWidth: true,
+            gutter: 20,
+            horizontalOrder: true,
+        });
+    });
+</script>
+
+
